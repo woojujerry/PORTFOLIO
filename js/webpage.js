245a0465f38f3d95563c46b90d1a064808f5e22e
woojujerry <140800032+woojujerry@users.noreply.github.com>
2023-11-28 10:03:05 +0900

    Update webpage.js

--- js/webpage.js
+++ js/webpage.js
@@ -1,28 +1,28 @@
 $(function (){
     // 말본골프
     $("#webpage01").mouseenter(function(){
-        $("#webpage01 img").stop().animate({marginTop:"-1890px"}, 4000)
+        $("#webpage01 img").stop().animate({marginTop:"-118em"}, 4000)
     }).mouseleave(function(){
         $("#webpage01 img").stop().animate({marginTop: "0px"}, 2000)
     })
 
     // 알라딘
     $("#webpage02").mouseenter(function(){
-        $("#webpage02 img").stop().animate({marginTop:"-1055px"}, 3000)
+        $("#webpage02 img").stop().animate({marginTop:"-66em"}, 3000)
     }).mouseleave(function(){
         $("#webpage02 img").stop().animate({marginTop: "0px"}, 1000)
     })
 
     // 에스파시오
     $("#webpage03").mouseenter(function(){
-        $("#webpage03 img").stop().animate({marginTop:"-1085px"}, 2000)
+        $("#webpage03 img").stop().animate({marginTop:"-68em"}, 2000)
     }).mouseleave(function(){
         $("#webpage03 img").stop().animate({marginTop: "0px"}, 500)
     })
 
     // 홈플러스
     $("#webpage04").mouseenter(function(){
-        $("#webpage04 img").stop().animate({marginTop:"-1567px"}, 3000)
+        $("#webpage04 img").stop().animate({marginTop:"-98em"}, 3000)
     }).mouseleave(function(){
         $("#webpage04 img").stop().animate({marginTop: "0px"}, 1000)
     })
@@ -38,7 +38,7 @@ $(function (){
 
     // 비디오포폴
     $("#webpage07").mouseenter(function(){
-        $("#webpage07 img").stop().animate({marginTop:"-1547px"}, 3000)
+        $("#webpage07 img").stop().animate({marginTop:"-96em"}, 3000)
     }).mouseleave(function(){
         $("#webpage07 img").stop().animate({marginTop: "0px"}, 1000)
     })
